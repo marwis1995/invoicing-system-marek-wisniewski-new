2c7ac995c8930dbd43175273d648a18b77deb087
Marek Wisniewski <a@a.pl>
2024-03-12 18:23:17 +0100

    Init commit

--- app/src/main/java/pl/futurecollars/invocing/App.java
+++ app/src/main/java/pl/futurecollars/invocing/App.java
@@ -4,11 +4,11 @@
 package pl.futurecollars.invocing;
 
 public class App {
-//    public String getGreeting() {
-//        return "Hello World!";
-//    }
+    public String getGreeting() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
-//        System.out.println(new App().getGreeting());
+        System.out.println(new App().getGreeting());
     }
 }
